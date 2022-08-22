0b9843f2ea33b5cccf41ca3e95c922fa6a1adb81
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-08-21 23:46:40 -0400

    added portfolio page, npm run is broken?

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AboutPage from './pages/AboutPage'
+import PortfolioPage from './pages/PortfolioPage';
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -14,6 +15,7 @@ function App() {
       <Container>
         <Routes>
         <Route path='/' element={<AboutPage />} />
+        <Route path='/portfolio' element={<PortfolioPage />} />
 
         </Routes>
       </Container>
---
+++ src/pages/PortfolioPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Image, Figure, Container, Card, Row, Col } from 'react-bootstrap'
+
+const PortfolioPage = () => {
+    return (
+        <Container>
+            Portfolio Page
+        </Container>
+    )
+}
+
+export default PortfolioPage
